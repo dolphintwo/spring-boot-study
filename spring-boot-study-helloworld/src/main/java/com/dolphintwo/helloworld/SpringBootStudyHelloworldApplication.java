2173e2f63d2d4d1953e73d2b97485ed0c1305d86
dolphintwo <dtdinghui@126.com>
2019-11-20 17:29:59 +0800

    init helloworld

---
+++ spring-boot-study-helloworld/src/main/java/com/dolphintwo/helloworld/SpringBootStudyHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.dolphintwo.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudyHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStudyHelloworldApplication.class, args);
+	}
+
+}
